06a9668b47860280c5a89e55c77ea694b391e6df
IronKnight525 <ironknight525@gmail.com>
2018-01-25 23:34:49 +0100

    CharacterController
    poprawiony skok

--- Character.cs
+++ Character.cs
@@ -2,63 +2,84 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Character : MonoBehaviour {
+public class Character : MonoBehaviour
+{
 
 
-    private Rigidbody rigidbody;
+    private Rigidbody rb;
     private string moveInputAxis = "Horizontal";
     public float moveSpeed = 2;
 
+    public float fallMultipler = 2.5f;
+    public float lowJumpMultipler = 2f;
 
-    private float jumpForce = 20.0f;
-    
-   
-    void Start ()
+    private float jumpForce = 5.0f;
+    int jumpCount=0;
+    bool grounded;
+    void Start()
     {
-        rigidbody = GetComponent<Rigidbody>();
-        controller = GetComponent<CharacterController>();
-	}
-	
-	void Update ()
+        rb = GetComponent<Rigidbody>();
+
+    }
+ 
+    void Update()
     {
         float moveAxis = Input.GetAxis(moveInputAxis);
-       
+    
+   
+        
         Move();
         Turn(moveAxis);
         Jump();
-       
-	}
+
+    }
     private void Move()
     {
         if (Input.GetButton(moveInputAxis))
         {
 
-            transform.Translate(new Vector3(0,0,1)*moveSpeed* Time.deltaTime);
+            transform.Translate(new Vector3(0, 0, 1) * moveSpeed * Time.deltaTime);
 
-          
+            
 
         }
     }
     private void Jump()
     {
-       
-            if(Input.GetButtonDown("Jump"))
-            {
-            transform.Translate(new Vector3(0, 1, 0) * jumpForce *Time.fixedDeltaTime);
-            }
-            
-        
-       
+        if (rb.velocity.y < 0)
+        {
+            rb.velocity += Vector3.up * Physics.gravity.y * (fallMultipler - 1) * Time.deltaTime;
+        }
+
+        if (Input.GetButtonDown("Jump")&&jumpCount<=1)
+        {
+            jumpCount++;
+            rb.velocity = Vector3.up * jumpForce;
+            grounded = false;
+        }
+        else if(grounded==true)
+        {
+            jumpCount = 0;
+        }
+
+
     }
     private void Turn(float input)
     {
-        if(input>0)
+        if (input > 0)
         {
             transform.rotation = Quaternion.Euler(0, -90, 0);
         }
-        if(input<0)
+        if (input < 0)
         {
             transform.rotation = Quaternion.Euler(0, 90, 0);
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.collider.tag=="floor")
+        {
+            grounded = true;
         }
     }
 }
