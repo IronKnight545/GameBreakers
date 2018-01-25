008806ceaf0df1cfde0ff52b82a7a865bc5404d7
IronKnight525 <ironknight525@gmail.com>
2018-01-25 19:47:32 +0100

    movingPlatform

---
+++ movingPlatform.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movingPlatform : MonoBehaviour
+{
+    public Rigidbody player;
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            other.transform.parent = transform;
+        }
+    }
+   
+
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.transform.tag == "Player")
+        {
+            other.transform.parent = null;
+
+        }
+    }
+}
