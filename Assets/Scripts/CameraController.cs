9c12dd16b551eb49b8a6db3095e2dc4cdc82ea28
IronKnight525 <ironknight525@gmail.com>
2018-01-27 13:35:58 +0100

    DeadZone i MovingCube
    DeadZone i MovingCube

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform sphere;
+
+
+
+    void Update()
+
+    {
+        Vector3 vector = new Vector3(0f, 4f, 7f);
+
+        Rigidbody rigidbody = sphere.GetComponent<Rigidbody>();
+        transform.position = sphere.position + vector;
+        float velocity = rigidbody.velocity.sqrMagnitude;
+        transform.LookAt(sphere);
+        vector = vector * (1f + velocity / 100f);
+        Vector3 newPosition = sphere.position + vector;
+        transform.position = Vector3.Lerp(transform.position, newPosition, Time.deltaTime * 2f);
+    }
+}
---
+++ Assets/Scripts/MovingCube.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingCube : MonoBehaviour
+{
+
+    Rigidbody rb;
+    private bool isPlayer;
+	void Start ()
+    {
+
+        rb = GetComponent<Rigidbody>();
+	}
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag != "floor")
+        {
+            rb.isKinematic = false;
+        }
+        else
+        {
+            rb.isKinematic = true;
+        }
+
+        if (other.tag=="Player")
+        {
+            isPlayer = true;
+        }
+      
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+           
+
+                transform.Translate(new Vector3(-1, 0, 0) * 5* Time.deltaTime);
+
+
+
+            
+        }
+        else
+        {
+            transform.Translate(new Vector3(0, 0, 0) * 5 * Time.deltaTime);
+        }
+       
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag.Equals("Player"))
+        {
+            isPlayer = false;
+        }
+
+    }
+    void OnGUI()
+    {
+        if (isPlayer)
+        {
+            GUI.contentColor = Color.black;
+            GUI.Label(new Rect(Screen.width / 2 - 100, Screen.height / 2 - 10, 100, 20), "Hold L-Shift to push","");
+        }
+    }
+}
