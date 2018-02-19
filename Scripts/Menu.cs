ab2af6c2c16b0c0ae6ccdff048339d37798ae3a9
IronKnight525 <ironknight525@gmail.com>
2018-02-19 16:55:32 +0100

    Menu, Scena I

---
+++ Scripts/Menu.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class Menu : MonoBehaviour
+{
+ 
+    
+    public Canvas menu;
+  
+  
+   
+   
+    void Start()
+    {
+           
+        menu = GetComponent<Canvas>();
+        menu.enabled = true;
+    }
+  
+
+    public void Play ()
+    {
+
+       SceneManager.LoadScene(0);
+    }
+   
+
+    public void Exit()
+    {
+        Application.Quit();
+
+    }
+    
+  
+}
