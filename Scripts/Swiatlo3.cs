0dfd962ad72af310cb0c0964cd11169172edbc3b
IronKnight525 <ironknight525@gmail.com>
2018-02-28 21:52:58 +0100

    Mapa complete

---
+++ Scripts/Swiatlo3.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Swiatlo3 : MonoBehaviour
+{
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+           
+         
+                SceneManager.LoadScene(SceneManager.GetSceneAt(0).name);
+            
+
+        }
+    }
+}
